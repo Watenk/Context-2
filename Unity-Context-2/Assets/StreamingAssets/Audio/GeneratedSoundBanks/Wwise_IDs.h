89355676d0e29521ce9fa9a44fd6d0ae42c27a71
Lelah van Eijnsbergen <lvaneijnsbergen@outlook.com>
2024-02-16 16:02:57 +0100

    Wwise Unity integration Set-up
    Wwise editor, soundbanks and unity integration

---
+++ Unity-Context-2/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,54 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID TEST_EVENT = 4258653052U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace NEW_STATE_GROUP
+        {
+            static const AkUniqueID GROUP = 2012657067U;
+
+            namespace STATE
+            {
+                static const AkUniqueID NEW_STATE = 2021425847U;
+                static const AkUniqueID NEW_STATE_01 = 3436006651U;
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace NEW_STATE_GROUP
+
+    } // namespace STATES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
