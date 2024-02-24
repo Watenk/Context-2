4e7dc0865463028025a63a549219cd60a58228a9
watenk <ivar.nuij@gmail.com>
2024-02-24 15:25:23 +0100

    Worked on AudioManager

--- Unity-Context-2/Assets/2_Scripts/AudioManager.cs
+++ Unity-Context-2/Assets/2_Scripts/AudioManager.cs
@@ -4,32 +4,71 @@ using UnityEngine;
 
 public class AudioManager : IUpdateable
 {
-    private GameObject soundObject;
-    private List<GameObject> soundObjects = new List<GameObject>();
+    private List<GameObject> sounds = new List<GameObject>();
 
     // References
     private InputManager inputManager;
+    private PlayerController player;
 
     //-----------------------------------------
 
     public AudioManager(){
         inputManager = GameManager.Instance.GetService<InputManager>();
+        inputManager.OnSquareDown += OnSquareDown;
+        inputManager.OnCircleDown += OnCircleDown;
+        inputManager.OnTriangleDown += OnTriangleDown;
+        inputManager.OnCrossDown += OnCrossDown;
+        inputManager.OnSquareUp += OnSquareUp;
+        inputManager.OnCircleUp += OnCircleUp;
+        inputManager.OnTriangleUp += OnTriangleUp;
+        inputManager.OnCrossUp += OnCrossUp;
+        player = GameManager.Instance.Player;
     }
 
     public void OnUpdate(){
 
     }
 
-    public void CallSoundEvent(AK.Wwise.Event wwiseEvent, Vector3 pos){
-        GameObject soundInstance = InstanceSound(pos);
-        soundInstance.transform.SetParent(GameManager.Instance.transform);
-        soundObjects.Add(soundInstance);
-        wwiseEvent.Post(soundInstance);
+    public void PlaySoundFor(float lenght){
+
     }
 
     //-----------------------------------
 
-    private GameObject InstanceSound(Vector3 pos){
-        return GameObject.Instantiate(soundObject, pos, Quaternion.identity);
+    private void InstanceSound(AK.Wwise.Event wwiseEvent, Vector3 pos){
+        GameObject soundInstance = GameObject.Instantiate(new GameObject(), pos, Quaternion.identity);
+        soundInstance.transform.SetParent(GameManager.Instance.transform);
+        sounds.Add(soundInstance);
+        wwiseEvent.Post(soundInstance);
+    }
+
+    private void OnSquareDown(){
+        InstanceSound(AudioSettings.Instance.PlayPlayerSquareChime, player.transform.position);
+    }
+
+    private void OnCircleDown(){
+        InstanceSound(AudioSettings.Instance.PlayPlayerCircleChime, player.transform.position);
+    }
+
+    private void OnTriangleDown(){
+        InstanceSound(AudioSettings.Instance.PlayPlayerTriangleChime, player.transform.position);
+    }
+
+    private void OnCrossDown(){
+    }
+
+    private void OnSquareUp(){
+        InstanceSound(AudioSettings.Instance.StopPlayerSquareChime, player.transform.position);
+    }
+
+    private void OnCircleUp(){
+        InstanceSound(AudioSettings.Instance.StopPlayerCircleChime, player.transform.position);
+    }
+
+    private void OnTriangleUp(){
+        InstanceSound(AudioSettings.Instance.StopPlayerTriangleChime, player.transform.position);
+    }
+
+    private void OnCrossUp(){
     }
 }
