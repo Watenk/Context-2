2e2f4db72e0b3ba5c05d7e29f813e80478e506bf
Mia vreven <39905553+naxmaardur@users.noreply.github.com>
2024-03-27 21:53:35 +0100

    Game Completion detection

---
+++ Unity-Context-2/Assets/2_Scripts/GameProgressTracker.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameProgressTracker : MonoBehaviour
+{
+    private int progress;
+
+
+
+    public void UpdateProgress()
+    {
+        progress++;
+        if(progress > 9)
+        {
+            StartCoroutine(DelayedEnding());
+        }
+    }
+
+    private void StartOutro()
+    {
+        FindObjectOfType<SceneSwitcher>().SwitchScene(3);
+    }
+
+    private IEnumerator DelayedEnding()
+    {
+        yield return new WaitForSeconds(4);
+        StartOutro();
+    }
+}
